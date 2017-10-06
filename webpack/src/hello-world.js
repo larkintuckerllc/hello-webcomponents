f150facaed41d913ac6ffd08a5033d77c431495c
John  Tucker <john@larkintuckerllc.com>
2017-10-06 17:43:30 -0400

    Fix

--- webpack/src/hello-world.js
+++ webpack/src/hello-world.js
@@ -4,7 +4,6 @@ window.addEventListener('WebComponentsReady', () => {
       super();
       const shadowRootEl = this.attachShadow({mode: 'open'});
       shadowRootEl.innerHTML = '<div>Hello World</div>';
-      const internalRootEl = shadowRootEl.getElementById('root');
     }
   }
   window.customElements.define('hello-world', HelloWorld);
